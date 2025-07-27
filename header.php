bd9509856f0b9575fffece6f254150a6d7f4994c
Nolan <nolan.sallai@protonmail.ch>
2025-07-27 12:51:00 +0200

    chore: remove tailwind dependencies

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
         <title><?php echo get_the_title(); ?> | <?php echo get_bloginfo('name'); ?> </title>
 
